28a2d7f82a0caaccb4c541f5861e49fd960b44be
tavitos <lesamaoyabi@gmail.com>
2023-06-13 12:50:30 -0600

    Instalación de primeNg e intro a pipes video 142 a la 147.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  // title = 'ocTaViO LezAma';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
+
 }
